448ffc47cd802a9789cd5f629cd352eda168050d
Guilherme Ferreira Lenzolari <guilhermeferreiralenzolari@10-40-48-210.0200.ecps.br.iosda.org>
2024-08-07 11:42:22 -0300

    starts project

---
+++ PortuguesePronunciation/PortuguesePronunciation/PortuguesePronunciationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortuguesePronunciationApp.swift
+//  PortuguesePronunciation
+//
+//  Created by Guilherme Ferreira Lenzolari on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PortuguesePronunciationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
