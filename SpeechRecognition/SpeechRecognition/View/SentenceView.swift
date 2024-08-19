48322115951f149e55338af79e6dd388ee40d867
Raphael Tavares Shimamoto <raphaelshimamoto@hotmail.com>
2024-08-19 11:24:15 -0300

    Sentence view arrumada

--- SpeechRecognition/SpeechRecognition/View/SentenceView.swift
+++ SpeechRecognition/SpeechRecognition/View/SentenceView.swift
@@ -9,82 +9,87 @@ import SwiftUI
 import Speech
 
 struct SentenceView: View {
+    
     var title = "Colors"
     var sentence = "Coração"
     var pronunciation = "Como se pronuncia"
-    var speechRecognizer = SpeechRecognizer(targetWord: "oi")
+    var speechRecognizer = SpeechRecognizer(targetWord: "Coração")
+    
     @State private var isRecording = false
-    @StateObject var audioPlayer = AudioPlayerViewModel(audio: "mao", audioFormat: "m4a")
     @State var sentanceCount = 0
+    @StateObject var audioPlayer = AudioPlayerViewModel(audio: "mao", audioFormat: "m4a")
     
     var body: some View {
-        NavigationView {
-            VStack {
-                
-                SegmentedProgressView(value: 0, maximum: 10)
-                
-                HStack{
-                    Text("Speak this sentence")
-                        .font(.title2)
-                        .bold()
-                        .padding()
-                    Spacer()
-                }
-                
-                HStack {
-                    Image(systemName: "tree.fill")
-                        .font(.title)
-                    
-                    VStack {
-                        HStack {
-                            Button(action: {
-                                audioPlayer.playOrPause()
-                            }) {
-                                Image(systemName:"speaker.wave.3.fill")
-                                    .font(.title)
-                                    .foregroundStyle(.black)
-                            }
-                            
-                            Text("\"\(sentence)\"")
-                                .font(.title)
-                        }
-                        Text("/\(pronunciation)/")
-                    }
-                    Spacer()
-                }
+        SegmentedProgressView(value: 0, maximum: 10)
+            .padding(.horizontal)
+        VStack {
+            HStack{
+                Text("Speak this sentence")
+                    .font(.title2)
+                    .bold()
+                    .padding()
                 
                 Spacer()
+            }
+            
+            HStack {
+                Image("birdStart")
+                    .resizable()
+                    .frame(width: 129, height: 130)
+                    .font(.title)
+                    .padding()
                 
-                Button(action: {
-                    if self.isRecording {
-                        self.speechRecognizer.stopSpeechRecording()
-                    } else {
-                        self.speechRecognizer.startSpeechRecording()
+                VStack {
+                    HStack {
+                        Button(action: {
+                            audioPlayer.playOrPause()
+                        }) {
+                            Image(systemName:"speaker.wave.3.fill", variableValue: audioPlayer.waveValue)
+                                .font(.title)
+                                .foregroundStyle(.blue)
+                        }
+                        
+                        Text("\"\(sentence)\"")
+                            .font(.title)
                     }
-                    self.isRecording.toggle()
-                }) {
-                    Text(isRecording ? "Stop Recording" : "Start Recording")
-                        .padding()
-                        .background(isRecording ? Color.red : Color.green)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                    Text("/\(pronunciation)/")
                 }
-                
-                Button(action: {
-                    self.speechRecognizer.audioRecorder.playRecording()
-                }) {
-                    Text("Play Recording")
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                Spacer()
+            }
+            
+            Spacer()
+            
+            //                Button(action: {
+            //                    self.speechRecognizer.audioRecorder.playRecording()
+            //                }) {
+            //                    Text("Play Recording")
+            //                        .padding()
+            //                        .background(Color.blue)
+            //                        .foregroundColor(.white)
+            //                        .cornerRadius(10)
+            //                }
+            
+            Button(action: {
+                if self.isRecording {
+                    self.speechRecognizer.stopSpeechRecording()
+                } else {
+                    self.speechRecognizer.startSpeechRecording()
                 }
-                
-            }.navigationTitle(title)
-                .toolbarTitleDisplayMode(.inline)
-                .padding()
+                self.isRecording.toggle()
+            }) {
+                Text("Touch to speak")
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .padding()
         }
-        
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Text(title)
+            }
+        }
+        .toolbarBackground(.white, for: .navigationBar)
+        .background(Color(UIColor.systemGroupedBackground))
     }
 }
 
